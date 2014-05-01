e918318a37f1e81a3627314b3ddf5c82ba220a85
Peter Stuart <peter@electricpeelsoftware.com>
2014-05-01 14:17:40 -0400

    Initial Commit

---
+++ EPSErrorHandling/EPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EPSAppDelegate.h
+//  EPSErrorHandling
+//
+//  Created by Peter Stuart on 5/1/14.
+//  Copyright (c) 2014 Electric Peel, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EPSErrorHandling/EPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSViewController.h
+//  EPSErrorHandling
+//
+//  Created by Peter Stuart on 5/1/14.
+//  Copyright (c) 2014 Electric Peel, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSViewController : UIViewController
+
+@end
